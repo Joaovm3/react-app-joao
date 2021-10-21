0693392961cc5b5b92f923c0bfa25bc216961c3d
João Victor Martins <joao.jovm@gmail.com>
2021-10-21 13:17:26 -0300

    feat: creating a input with icon component

---
+++ src/components/InputWithIcon/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box,  FormControl, InputLabel } from '@mui/material';
+
+import { InputStyle } from './style';
+
+export default function InputWithIcon({ children , type = "text", label = "Default", ...rest }) {
+    return (
+        <Box sx={{ display: "flex", alignItems: "flex-end", margin: '15px 0px' }}>
+            {children}
+
+            <FormControl>
+                <InputLabel> {label} </InputLabel>
+                <InputStyle 
+                    {...rest} 
+                    type={type} 
+                    variant="standard" 
+                />
+            </FormControl>
+        </Box>
+    );
+}
---
+++ src/components/InputWithIcon/style.js
@@ -0,0 +1,10 @@
+//MuiFormControl-root
+
+import { Input } from "@mui/material";
+import styled from "styled-components";
+
+export const InputStyle = styled(Input)`
+  &.MuiInput-underline {
+    margin-top: 7px;
+  }
+`;
