6b2ca76f0d16927976707ac6c9c19ac38d532733
João Victor Martins <joao.jovm@gmail.com>
2021-10-21 17:51:56 -0300

    feat(login): implemented form validation

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,50 +1,48 @@
-import React, { useState } from "react";
-import { useFormik } from "formik";
+import React, { useState, useEffect } from "react";
+import { Formik, useFormik, Form } from "formik";
 
-import {
-  BodyStyle,
-  ContainerStyle,
-  ButtonStyle,
-  LoginStyle,
-} from "./style";
+import { BodyStyle, ContainerStyle, ButtonStyle, LoginStyle } from "./style";
 
 import { Person, Visibility, VisibilityOff, Lock } from "@material-ui/icons";
 
 import {
+  Link,
   InputAdornment,
   IconButton,
-  OutlinedInput,
-  Input as Ipt,
+  FormHelperText 
 } from "@mui/material";
 
 import * as Yup from "yup";
 
-import Input from "../../components/Input";
 import Logo from "../../components/Logo";
 import CheckBoxWithLabel from "../../components/CheckBoxWithLabel";
 import InputWithIcon from "../../components/InputWithIcon";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
+  const [disableButton, setDisableButton] = useState(true);
 
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
   };
 
   const handleErrorFormik = (error) => {
-    return (
-      <div style={{color: 'red', fontSize: '10px'}}> {error} </div>
-    );
-  }
+    return <FormHelperText style={{ color: "red", fontSize: "10px" }}> {error} </FormHelperText>;
+  };
+
+  const pattern = /^[a-z0-9]+$/i; 
+  const minSevenNumbers = 700000;
 
   const validationSchema = Yup.object().shape({
     user: Yup.string()
-      .min(1, "O campo deve ser maior que 1 caractere!")
-      .required("Campo obrigatório!"),
+    .min(2, "O campo deve ter no mínimo 2 caracteres!")
+    .matches(pattern, "Não pode haver caracteres especiais!")
+    .required("Campo obrigatório!"),
     password: Yup.number()
-      .positive()
-      .integer()
-      .min(7, "A senha deve ter no mínimo 7 número!")
+      .typeError("A senha deve ser um número!")
+      .positive("Deve ser um número positivo")
+      .integer("Deve ser um número inteiro")
+      .min(minSevenNumbers, "A senha deve ter no mínimo 7 números!")
       .required("Campo obrigatório!"),
   });
 
@@ -54,89 +52,88 @@ export default function Login() {
       password: "",
     },
     onSubmit: (values) => {
+    /* setTimeout(() => {
+        
+      }, 3000);
+    */
       alert(JSON.stringify(values, null, 2));
     },
-    /* validationSchema:{validationSchema}, */
-    validate: ({ user, password }) => {
-      const error = {};
-
-      if (user.length <= 1) {
-        error.user = "O campo deve ser maior que 1 caractere!";
-      }
-
-      if (!user) {
-        error.user = "Campo não preenchido!";
-      }
-
-      if (password.length < 7) {
-        error.password = "A senha deve ter no mínimo 7 caracteres!";
-      }
-
-      if (!password) {
-        error.password = "Campo não preenchido!";
-      }
-
-      return error;
-    },
+    validationSchema:validationSchema,
   });
+
+  useEffect(() => {
+    if (formik.isValid) {
+      return setDisableButton(false)
+    }
+    return setDisableButton(true)
+  }, [formik.isValid]);
 
   return (
     <BodyStyle>
       <ContainerStyle>
         <Logo />
+
         <LoginStyle> LOGIN </LoginStyle>
-        <form onSubmit={formik.handleSubmit}>
-          <div style={{ margin: "5px" }}>
-            
-            <InputWithIcon
-              error={formik.errors.user}
-              name="user"
-              label="Usuário"
-              value={formik.values.user}
-              onChange={formik.handleChange}
-              /* onBlur={(value)=>console.log(value)} */
-              fullWidth
-            > 
-              <Person sx={{ color: "action.active", mr: 1, my: 0.5 }} /> 
-            </InputWithIcon>
 
-            {formik.errors.user && handleErrorFormik(formik.errors.user)}
+        <Formik
+          initialValues={formik.initialValues}
+          validationSchema={validationSchema}
+        >
+          {(props) => (
+            <Form onSubmit={formik.handleSubmit}>
+              <div style={{ margin: "5px" }}>
+                <InputWithIcon
+                  error={formik.errors.user}
+                  name="user"
+                  label="Usuário"
+                  value={formik.values.user}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleChange}
+                  fullWidth
+                >
+                  <Person />
+                </InputWithIcon>
 
-            <InputWithIcon
-              error={formik.errors.password}
-              helperText={formik.errors.password}
-              name="password"
-              label="Senha"
-              type={showPassword ? "text" : "password"}
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={handleClickShowPassword}
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            > 
-              <Lock sx={{ color: "action.active", mr: 1, my: 0.5 }} /> 
-            </InputWithIcon>
-            
-            {formik.errors.password && handleErrorFormik(formik.errors.password)} 
+                {formik.errors.user && handleErrorFormik(formik.errors.user)}
 
-            <CheckBoxWithLabel />
+                <InputWithIcon
+                  error={formik.errors.password}
+                  name="password"
+                  label="Senha"
+                  type={showPassword ? "text" : "password"}
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleChange}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton onClick={handleClickShowPassword}>
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                >
+                  <Lock />
+                </InputWithIcon>
 
-            <ButtonStyle
-              type="submit"
-              variant="contained"
-              color="primary"
-              fullWidth
-            >
-              Acessar
-            </ButtonStyle>
-          </div>
-        </form>
+                {formik.errors.password && handleErrorFormik(formik.errors.password)}
+
+                <CheckBoxWithLabel />
+                  
+                <ButtonStyle
+                  disabled={disableButton}
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Acessar
+                </ButtonStyle>
+
+                <p style={{ fontSize: '10px', opacity: 0.5 }}> Esqueceu sua senha? <Link href="/esqueci-minha-senha"> Clique Aqui </Link> </p>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </ContainerStyle>
     </BodyStyle>
   );
