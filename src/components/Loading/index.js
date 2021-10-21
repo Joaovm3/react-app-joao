0698c5aae0acd28fb9bab017ec69cc101ce1a81e
João Victor Martins <joao.jovm@gmail.com>
2021-10-21 17:52:55 -0300

    feat: wip loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { CircularProgress, Box } from '@mui/material';
+
+export default function Loading() {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CircularProgress />
+        </Box>
+    )
+}
