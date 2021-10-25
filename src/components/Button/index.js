93135c7cc46b3199d2594128eb5d86243f0cba7c
João Victor Martins <joao.jovm@gmail.com>
2021-10-25 00:28:55 -0300

    fix(button): spread props into button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Button as Btn } from '@material-ui/core';
 
-export default function Button({ children, variant = 'contained', color = 'primary' }) {
+export default function Button({ children, variant = 'contained', color = 'primary', disabled = false, ...props }) {
     return (
-        <Btn variant={variant} color={color}>
+        <Btn variant={variant} color={color} disabled={disabled} {...props}>
             {children}
         </Btn>
     );
